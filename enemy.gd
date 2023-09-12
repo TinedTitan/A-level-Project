5e41973860d7fa1566afcbcb67d1b5e76ecc7655
TinedTitan <lukewdm1@gmail.com>
2023-09-12 14:01:01 +0100

    Added 2D navigation for ai

--- enemy.gd
+++ enemy.gd
@@ -6,22 +6,37 @@ var speed = 400
 var player_chase = false
 var player = null
 
+func _ready():
+	$NavigationAgent2D.avoidance_enabled
+
 func _on_sightline_body_entered(body):
 	player = body
-	
+	$NavigationAgent2D.set_target_position(player.global_position)
 	player_chase = true
 
 func _on_sightline_body_exited(body):
 	player = null
+	
 	player_chase = false
 
 func _physics_process(delta):
 	if player_chase == true:
 		
-		var direction = (player.position - position).normalized()
-		look_at(player.global_position)
-		set_velocity(direction * speed)
-		move_and_slide()
+		$NavigationAgent2D.set_target_position(player.global_position)
+		
+		if not $NavigationAgent2D.is_navigation_finished(): #if the enemy hasn't reached the player
+			var current_agent_position = get_global_position()
+			
+			var next_path_position = $NavigationAgent2D.get_next_path_position()
+			velocity = (next_path_position - current_agent_position).normalized() * speed
+			
+			look_at(player.global_position)
+			move_and_slide()
+		
+		#var direction = (player.position - position).normalized()
+		#look_at(player.global_position)
+		#set_velocity(direction * speed)
+		#move_and_slide()
 		
 		if velocity.x != 0 or velocity.y != 0:
 			$AnimatedSprite2D.play("Moving")
