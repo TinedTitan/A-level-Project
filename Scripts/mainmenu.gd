e005dc9a9bdfef08b3a50c102643077d3c86370f
TinedTitan <lukewdm1@gmail.com>
2024-01-19 12:25:05 +0000

    Pause Menu

--- Scripts/mainmenu.gd
+++ Scripts/mainmenu.gd
@@ -1,14 +1,10 @@
 extends Control
 
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Maps/level1.tscn")
 
 func _on_settings_pressed():
 	get_tree().change_scene_to_file("res://Main Menu/settings.tscn")
-
-func _ready():
-	
 
 func _on_exit_pressed():
 	get_tree().quit()
